d963f432c922e795702513c995432eb7620ef9ac
RottenZelka <mishoitedis@gmail.com>
2024-06-13 17:10:39 +0300

    app react

--- frontend/screens/App.tsx
+++ frontend/screens/App.tsx
@@ -1,13 +1,12 @@
-// App.tsx
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, Button, StyleSheet } from 'react-native';
+
 import SignUpScreen from './SignUpScreen';
 import LoginScreen from './LogInScreen';
 import SchoolSignUpScreen from './SchoolSignUpScreen';
 import SchoolLogInScreen from './SchoolLogInScreen';
-import { View, Button, StyleSheet } from 'react-native';
 import StudentHomeScreen from './StudentHomeScreen';
 import ApplyForSchoolScreen from './ApplyForSchoolScreen';
 import SchoolDetailScreen from './SchoolDetailScreen';
@@ -17,49 +16,47 @@ import EditInfoScreen from './EditInfoScreen';
 import ViewApplicantsScreen from './ViewApplicantsScreen';
 import StudentDetailScreen from './StudentDetailScreen';
 
-
 const Stack = createStackNavigator();
 
-const App = () => {
+const App: React.FC = () => {
   return (
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Welcome">
-            <Stack.Screen
-              name="Welcome"
-              component={WelcomeScreen}
-              options={{ title: 'Welcome' }}
-            />
-            <Stack.Screen
-              name="SignUp"
-              component={SignUpScreen}
-              options={{ title: 'Sign Up' }}
-            />
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{ title: 'Login' }}
-            />
-            <Stack.Screen
-              name="SchoolSignUp"
-              component={SchoolSignUpScreen} // Add SchoolSignUpScreen as a new screen
-              options={{ title: 'School Sign Up' }}
-            />
-            <Stack.Screen
-              name="SchoolLogIn"
-              component={SchoolLogInScreen} // Add SchoolSignUpScreen as a new screen
-              options={{ title: 'School Log In' }}
-            />
-            <Stack.Screen name="StudentHomeScreen" component={StudentHomeScreen} />
-            <Stack.Screen name="ApplyForSchoolScreen" component={ApplyForSchoolScreen} />
-            <Stack.Screen name="SchoolDetailScreen" component={SchoolDetailScreen} />
-            <Stack.Screen name="SchoolHomeScreen" component={SchoolHomeScreen} />
-            <Stack.Screen name="SchoolEditScreen" component={SchoolEditScreen} />
-            <Stack.Screen name="EditInfoScreen" component={EditInfoScreen} />
-            <Stack.Screen name="ViewApplicantsScreen" component={ViewApplicantsScreen} />
-            <Stack.Screen name="StudentDetailScreen" component={StudentDetailScreen} />
-            {/* <Stack.Screen name="EditStudiesScreen" component={EditStudiesScreen} /> */}
-          </Stack.Navigator>
-        </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ title: 'Sign Up' }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Login' }}
+        />
+        <Stack.Screen
+          name="SchoolSignUp"
+          component={SchoolSignUpScreen}
+          options={{ title: 'School Sign Up' }}
+        />
+        <Stack.Screen
+          name="SchoolLogIn"
+          component={SchoolLogInScreen}
+          options={{ title: 'School Log In' }}
+        />
+        <Stack.Screen name="StudentHomeScreen" component={StudentHomeScreen} />
+        <Stack.Screen name="ApplyForSchoolScreen" component={ApplyForSchoolScreen} />
+        <Stack.Screen name="SchoolDetailScreen" component={SchoolDetailScreen} />
+        <Stack.Screen name="SchoolHomeScreen" component={SchoolHomeScreen} />
+        <Stack.Screen name="SchoolEditScreen" component={SchoolEditScreen} />
+        <Stack.Screen name="EditInfoScreen" component={EditInfoScreen} />
+        <Stack.Screen name="ViewApplicantsScreen" component={ViewApplicantsScreen} />
+        <Stack.Screen name="StudentDetailScreen" component={StudentDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
